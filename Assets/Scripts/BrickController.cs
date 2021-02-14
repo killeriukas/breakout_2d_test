dbe12c2bbc53f33e9618d364f67862b360b862ff
Arnoldas Gurskis <agurskis@22cans.com>
2021-02-14 01:24:14 +0000

    renamed brik controller to brick controller;

---
+++ Assets/Scripts/BrickController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class BrickController : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        GamePlay.instance.MarkAsScored();
+        Destroy(gameObject);
+    }
+
+}
