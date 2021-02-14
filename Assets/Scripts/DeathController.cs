ff27fc2800b1d21f8cd92a10702c8aed6fc18a72
Arnoldas Gurskis <agurskis@22cans.com>
2021-02-14 01:25:02 +0000

    renamed Death to Death controller;

---
+++ Assets/Scripts/DeathController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DeathController : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        GamePlay gameManager = GamePlay.instance;
+
+        gameManager.MarkAsDied();
+
+        if(gameManager.HasEnoughLives) {
+            gameManager.Restart();
+        }
+        
+    }
+
+}
